77478a0ef64afb5ee7f4770294b6758b11ee0d27
Lou DeGenaro <lou.degenaro@gmail.com>
2023-09-07 08:59:33 -0400

    Update scripts
    Signed-off-by: Lou DeGenaro <lou.degenaro@gmail.com>

--- scripts/automation/check_and_update_all.sh
+++ scripts/automation/check_and_update_all.sh
@@ -6,10 +6,10 @@ md_changed=false
 json_changed=false
 
 # bash regex does not support lazy match, so need to use two patterns to match before and after the control id
-md1=$"^md_catalogs/"
+md1=$"^md_profiles/"
 md2=$"\.md$"
 
-json1=$"^catalogs/"
+json1=$"^profiles/"
 json2=$"\.json$"
 
 for val in ${CHANGES[@]} ; do
@@ -24,15 +24,13 @@ done
 
 if [[ $json_changed = true ]]; then
     echo "Json file(s) were changed, regenerating markdowns..."
-    ./scripts/automation/regenerate_catalogs.sh
+    ./scripts/automation/regenerate_profiles.sh
 fi
 
 
 if [[ $md_changed = true ]]; then
-    echo "Md file(s) were changed, assembling catalog..."
-    ./scripts/automation/assemble_catalogs.sh
+    echo "Md file(s) were changed, assembling profile..."
+    ./scripts/automation/assemble_profiles.sh
 fi
-
-
 
 echo "$md_changed $json_changed changed"
